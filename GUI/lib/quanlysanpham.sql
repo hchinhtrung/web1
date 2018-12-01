08e8129a849ed0635b8e28c58a4cd5161103402a
chinh trung <chinhtrung1998@gmail.com>
2018-12-01 19:23:44 +0700

    // update database quanlysanpham

---
+++ GUI/lib/quanlysanpham.sql
@@ -0,0 +1,248 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Dec 01, 2018 at 12:09 PM
+-- Server version: 5.7.23
+-- PHP Version: 5.6.38
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `quanlysanpham`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chitietdonhang`
+--
+
+DROP TABLE IF EXISTS `chitietdonhang`;
+CREATE TABLE IF NOT EXISTS `chitietdonhang` (
+  `MaChiTietDonDatHang` varchar(11) COLLATE utf8_unicode_ci NOT NULL,
+  `SoLuong` int(11) NOT NULL,
+  `GiaBan` int(11) NOT NULL,
+  `MaDonDatHang` varchar(9) COLLATE utf8_unicode_ci NOT NULL,
+  `MaSanPham` int(11) NOT NULL,
+  PRIMARY KEY (`MaChiTietDonDatHang`),
+  KEY `MaSanPham` (`MaSanPham`),
+  KEY `MaDonHangDaDat` (`MaDonDatHang`),
+  KEY `MaDonDatHang` (`MaDonDatHang`),
+  KEY `MaDonDatHang_2` (`MaDonDatHang`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chitietsp`
+--
+
+DROP TABLE IF EXISTS `chitietsp`;
+CREATE TABLE IF NOT EXISTS `chitietsp` (
+  `MaChitietSP` int(11) NOT NULL AUTO_INCREMENT,
+  `TenSP` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `ManHinh` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `CamTruoc` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `CamSau` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `RAM` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `ROM` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `CPU` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `GPU` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `Pin` int(11) NOT NULL,
+  `HeDieuHanh` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `Sim` int(11) NOT NULL,
+  PRIMARY KEY (`MaChitietSP`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dondathang`
+--
+
+DROP TABLE IF EXISTS `dondathang`;
+CREATE TABLE IF NOT EXISTS `dondathang` (
+  `MaDonDatHang` varchar(9) COLLATE utf8_unicode_ci NOT NULL,
+  `NgayLap` datetime NOT NULL,
+  `TongThanhTien` int(11) NOT NULL,
+  `MaTaiKhoan` int(11) NOT NULL,
+  `MaTinhTrang` int(11) NOT NULL,
+  PRIMARY KEY (`MaTinhTrang`),
+  KEY `MaTaiKhoan` (`MaTaiKhoan`),
+  KEY `MaTaiKhoan_2` (`MaTaiKhoan`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hangsanxuat`
+--
+
+DROP TABLE IF EXISTS `hangsanxuat`;
+CREATE TABLE IF NOT EXISTS `hangsanxuat` (
+  `MaHangSanXuat` int(11) NOT NULL AUTO_INCREMENT,
+  `TenHangSanXuat` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `LogoURL` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `BiXoa` tinyint(1) NOT NULL,
+  PRIMARY KEY (`MaHangSanXuat`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hangsxcualoaisp`
+--
+
+DROP TABLE IF EXISTS `hangsxcualoaisp`;
+CREATE TABLE IF NOT EXISTS `hangsxcualoaisp` (
+  `MaLoaiSanPham` int(11) NOT NULL AUTO_INCREMENT,
+  `MaHangSanXuat` int(11) NOT NULL,
+  PRIMARY KEY (`MaLoaiSanPham`,`MaHangSanXuat`),
+  KEY `MaLoaiSanPham` (`MaLoaiSanPham`),
+  KEY `MaHangSanXuat` (`MaHangSanXuat`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `loaisanpham`
+--
+
+DROP TABLE IF EXISTS `loaisanpham`;
+CREATE TABLE IF NOT EXISTS `loaisanpham` (
+  `MaLoaiSanPham` int(11) NOT NULL AUTO_INCREMENT,
+  `TenLoaiSanPham` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `BiXoa` tinyint(1) NOT NULL,
+  PRIMARY KEY (`MaLoaiSanPham`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `loaitaikhoan`
+--
+
+DROP TABLE IF EXISTS `loaitaikhoan`;
+CREATE TABLE IF NOT EXISTS `loaitaikhoan` (
+  `MaLoaiTaiKhoan` int(11) NOT NULL AUTO_INCREMENT,
+  `TenLoaiTaiKhoan` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`MaLoaiTaiKhoan`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sanpham`
+--
+
+DROP TABLE IF EXISTS `sanpham`;
+CREATE TABLE IF NOT EXISTS `sanpham` (
+  `MaSanPham` int(11) NOT NULL AUTO_INCREMENT,
+  `TenSanPham` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `HinhURL` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `GiaSanPham` int(11) NOT NULL,
+  `NgayNhap` datetime NOT NULL,
+  `SoLuongTon` int(11) NOT NULL,
+  `SoLuongBan` int(11) NOT NULL,
+  `SoLuotXem` int(11) NOT NULL,
+  `MoTa` text COLLATE utf8_unicode_ci NOT NULL,
+  `BiXoa` tinyint(1) NOT NULL,
+  `MaLoaiSanPham` int(11) NOT NULL,
+  `MaHangSanXuat` int(11) NOT NULL,
+  `MaChitietSP` int(11) NOT NULL,
+  PRIMARY KEY (`MaSanPham`),
+  KEY `MaLoaiSanPham` (`MaLoaiSanPham`),
+  KEY `FK_HANGSANXUAT` (`MaHangSanXuat`),
+  KEY `MaChitietSP` (`MaChitietSP`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `taikhoan`
+--
+
+DROP TABLE IF EXISTS `taikhoan`;
+CREATE TABLE IF NOT EXISTS `taikhoan` (
+  `MaTaiKhoan` int(11) NOT NULL AUTO_INCREMENT,
+  `TenDangNhap` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `MatKhau` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `TenHienThi` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `DiaChi` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
+  `DienThoai` varchar(11) COLLATE utf8_unicode_ci NOT NULL,
+  `Email` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `BiXoa` tinyint(1) NOT NULL,
+  `MaLoaiTaiKhoan` int(11) NOT NULL,
+  PRIMARY KEY (`MaTaiKhoan`),
+  KEY `MaLoaiTaiKhoan` (`MaLoaiTaiKhoan`),
+  KEY `MaLoaiTaiKhoan_2` (`MaLoaiTaiKhoan`),
+  KEY `MaLoaiTaiKhoan_3` (`MaLoaiTaiKhoan`),
+  KEY `MaLoaiTaiKhoan_4` (`MaLoaiTaiKhoan`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tinhtrang`
+--
+
+DROP TABLE IF EXISTS `tinhtrang`;
+CREATE TABLE IF NOT EXISTS `tinhtrang` (
+  `MaTinhTrang` int(11) NOT NULL AUTO_INCREMENT,
+  `TenTinhTrang` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`MaTinhTrang`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `chitietdonhang`
+--
+ALTER TABLE `chitietdonhang`
+  ADD CONSTRAINT `FK_MASP` FOREIGN KEY (`MaSanPham`) REFERENCES `sanpham` (`MaSanPham`);
+
+--
+-- Constraints for table `dondathang`
+--
+ALTER TABLE `dondathang`
+  ADD CONSTRAINT `FK_MATAIKHOAN` FOREIGN KEY (`MaTaiKhoan`) REFERENCES `taikhoan` (`MaTaiKhoan`),
+  ADD CONSTRAINT `FK_MATINHTRANG` FOREIGN KEY (`MaTinhTrang`) REFERENCES `tinhtrang` (`MaTinhTrang`);
+
+--
+-- Constraints for table `hangsxcualoaisp`
+--
+ALTER TABLE `hangsxcualoaisp`
+  ADD CONSTRAINT `FK_HANGSX` FOREIGN KEY (`MaHangSanXuat`) REFERENCES `hangsanxuat` (`MaHangSanXuat`),
+  ADD CONSTRAINT `FK_LOAISP` FOREIGN KEY (`MaLoaiSanPham`) REFERENCES `loaisanpham` (`MaLoaiSanPham`);
+
+--
+-- Constraints for table `sanpham`
+--
+ALTER TABLE `sanpham`
+  ADD CONSTRAINT `FK_HANGSANXUAT` FOREIGN KEY (`MaHangSanXuat`) REFERENCES `hangsanxuat` (`MaHangSanXuat`),
+  ADD CONSTRAINT `FK_LOAISANPHAM` FOREIGN KEY (`MaLoaiSanPham`) REFERENCES `loaisanpham` (`MaLoaiSanPham`),
+  ADD CONSTRAINT `FK_MACHITIETSP` FOREIGN KEY (`MaChitietSP`) REFERENCES `chitietsp` (`MaChitietSP`);
+
+--
+-- Constraints for table `taikhoan`
+--
+ALTER TABLE `taikhoan`
+  ADD CONSTRAINT `FK_LOAITK` FOREIGN KEY (`MaLoaiTaiKhoan`) REFERENCES `loaitaikhoan` (`MaLoaiTaiKhoan`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
