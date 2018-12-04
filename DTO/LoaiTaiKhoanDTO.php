ec1fed61c6ebf4a8d0144c6db5c29a951147e82b
chinh trung <chinhtrung1998@gmail.com>
2018-12-05 00:39:23 +0700

    update ChiTietSP TinhTrang LoaiTaiKhoan trong folder DTO

---
+++ DTO/LoaiTaiKhoanDTO.php
@@ -0,0 +1,12 @@
+<?php
+    class LoaiTaiKhoanDTO
+    {
+        var $MaLoaiTaiKhoan;
+        var $TenLoaiTaiKhoan;
+        public function __construct()
+        {
+            $this->MaLoaiTaiKhoan = 0;
+            $this->TenLoaiTaiKhoan = '';
+        }
+    }
+?>
