7214f0319d9b2fc4d00beda2155891b43dd535a9
chinh trung <chinhtrung1998@gmail.com>
2018-12-06 12:02:58 +0700

    update Apple, Samsung, Nokia trong pIndex.php

--- GUI/pages/pIndex.php
+++ GUI/pages/pIndex.php
@@ -1,5 +1,6 @@
+
 <div id="box">
-        <img src="GUI/images/apple/iphoneXsmax1.png">
+        <img src="GUI/images/apple/iphoneXsmax.png">
         <p>
             Iphone XS Max
         </p>
@@ -8,63 +9,386 @@
         </div>
 </div>
 <div id="box">
-        <img src="GUI/images/apple/iphoneXsmax1.png">
+        <img src="GUI/images/apple/iphoneXs.png">
         <p>
-            Iphone XS Max
+            Iphone XS
         </p>
         <div>
             10.000.000 vnd
         </div>
 </div>
 <div id="box">
-        <img src="GUI/images/apple/iphoneXsmax1.png">
+        <img src="GUI/images/apple/iphonexr.png">
         <p>
-            Iphone XS Max
+            Iphone XR
         </p>
         <div>
             10.000.000 vnd
         </div>
 </div>
 <div id="box">
-        <img src="GUI/images/apple/iphoneXsmax1.png">
+        <img src="GUI/images/apple/iphonex.png">
         <p>
-            Iphone XS Max
+            Iphone X
         </p>
         <div>
             10.000.000 vnd
         </div>
 </div>
 <div id="box">
-        <img src="GUI/images/apple/iphoneXsmax1.png">
+        <img src="GUI/images/apple/iphone8plus.png">
         <p>
-            Iphone XS Max
+            Iphone 8 Plus
         </p>
         <div>
             10.000.000 vnd
         </div>
 </div>
 <div id="box">
-        <img src="GUI/images/apple/iphoneXsmax1.png">
+        <img src="GUI/images/apple/iphone8.png">
         <p>
-            Iphone XS Max
+            Iphone 8
         </p>
         <div>
             10.000.000 vnd
         </div>
 </div>
 <div id="box">
-        <img src="GUI/images/apple/iphoneXsmax1.png">
+        <img src="GUI/images/apple/iphone7plus.png">
         <p>
-            Iphone XS Max
+            Iphone 7 Plus
         </p>
         <div>
             10.000.000 vnd
         </div>
 </div>
 <div id="box">
-        <img src="GUI/images/apple/iphoneXsmax1.png">
+        <img src="GUI/images/apple/iphone7.png">
         <p>
-            Iphone XS Max
+            Iphone 7
+        </p>
+        <div>
+            10.000.000 vnd
+        </div>
+</div>
+<div id="box">
+        <img src="GUI/images/apple/iphone6plus.png">
+        <p>
+            Iphone 6 Plus
+        </p>
+        <div>
+            10.000.000 vnd
+        </div>
+</div>
+<div id="box">
+        <img src="GUI/images/apple/iphone6.png">
+        <p>
+            Iphone 6
+        </p>
+        <div>
+            10.000.000 vnd
+        </div>
+</div>
+<div id="box">
+        <img src="GUI/images/apple/iPadPro12.9Wifi4G.png">
+        <p>
+            iPad Pro 12.9 Wifi 4G 
+        </p>
+        <div>
+            10.000.000 vnd
+        </div>
+</div>
+<div id="box">
+        <img src="GUI/images/apple/iPadPro12.9Wifi.png">
+        <p>
+            iPad Pro 12.9 Wifi
+        </p>
+        <div>
+            10.000.000 vnd
+        </div>
+</div>
+<div id="box">
+        <img src="GUI/images/apple/iPadPro10.5Wifi4G.png">
+        <p>
+            iPad Pro 10.5 Wifi 4G
+        </p>
+        <div>
+            10.000.000 vnd
+        </div>
+</div>
+<div id="box">
+        <img src="GUI/images/apple/iPadPro10.5Wifi.png">
+        <p>
+            iPad Pro 10.5 Wifi
+        </p>
+        <div>
+            10.000.000 vnd
+        </div>
+</div>
+
+<div id="box">
+        <img src="GUI/images/apple/iPad2018wifi4G.png">
+        <p>
+            iPad 2018 Wifi 4G
+        </p>
+        <div>
+            10.000.000 vnd
+        </div>
+</div>
+<div id="box">
+        <img src="GUI/images/apple/iPad2018wifi.png">
+        <p>
+            iPad 2018 Wifi
+        </p>
+        <div>
+            10.000.000 vnd
+        </div>
+</div>
+<!--SAMSUNG-->
+
+<div id="box">
+        <img src="GUI/images/samsung/galaxya6plus.jpg">
+        <p>
+            Samsung Galaxy A6 Plus
+        </p>
+        <div>
+            10.000.000 vnd
+        </div>
+</div>
+<div id="box">
+        <img src="GUI/images/samsung/galaxya7.jpg">
+        <p>
+            Samsung Galaxy A7
+        </p>
+        <div>
+            10.000.000 vnd
+        </div>
+</div>
+<div id="box">
+        <img src="GUI/images/samsung/galaxya8.jpg">
+        <p>
+            Samsung Galaxy A8
+        </p>
+        <div>
+            10.000.000 vnd
+        </div>
+</div>
+<div id="box">
+        <img src="GUI/images/samsung/galaxya8star.jpg">
+        <p>
+            Samsung Galaxy A8 Star
+        </p>
+        <div>
+            10.000.000 vnd
+        </div>
+</div>
+
+<div id="box">
+        <img src="GUI/images/samsung/galaxynote8.jpg">
+        <p>
+            Samsung Galaxy Note 8
+        </p>
+        <div>
+            10.000.000 vnd
+        </div>
+</div>
+<div id="box">
+        <img src="GUI/images/samsung/galaxynote9.jpg">
+        <p>
+            Samsung Galaxy Note 9
+        </p>
+        <div>
+            10.000.000 vnd
+        </div>
+</div>
+<div id="box">
+        <img src="GUI/images/samsung/galaxynotefe.jpg">
+        <p>
+            Samsung Galaxy Note FE
+        </p>
+        <div>
+            10.000.000 vnd
+        </div>
+</div>
+<div id="box">
+        <img src="GUI/images/samsung/galaxys8.jpg">
+        <p>
+            Samsung Galaxy S8
+        </p>
+        <div>
+            10.000.000 vnd
+        </div>
+</div>
+<div id="box">
+        <img src="GUI/images/samsung/galaxys8plus.jpg">
+        <p>
+            Samsung Galaxy S8 Plus
+        </p>
+        <div>
+            10.000.000 vnd
+        </div>
+</div>
+<div id="box">
+        <img src="GUI/images/samsung/galaxys9.jpg">
+        <p>
+            Samsung Galaxy S9
+        </p>
+        <div>
+            10.000.000 vnd
+        </div>
+</div>
+<div id="box">
+        <img src="GUI/images/samsung/galaxys9.jpg">
+        <p>
+            Samsung Galaxy S9 Plus
+        </p>
+        <div>
+            10.000.000 vnd
+        </div>
+</div>
+
+<div id="box">
+        <img src="GUI/images/samsung/tabA6.jpg">
+        <p>
+            Samsung Galaxy Tab A6
+        </p>
+        <div>
+            10.000.000 vnd
+        </div>
+</div>
+<div id="box">
+        <img src="GUI/images/samsung/tabA7.jpg">
+        <p>
+            Samsung Galaxy Tab A7
+        </p>
+        <div>
+            10.000.000 vnd
+        </div>
+</div>
+<div id="box">
+        <img src="GUI/images/samsung/tabA8.0.jpg">
+        <p>
+            Samsung Galaxy Tab A8.0
+        </p>
+        <div>
+            10.000.000 vnd
+        </div>
+</div>
+<div id="box">
+        <img src="GUI/images/samsung/tabA10.5.jpg">
+        <p>
+            Samsung Galaxy Tab A10.5
+        </p>
+        <div>
+            10.000.000 vnd
+        </div>
+</div>
+<div id="box">
+        <img src="GUI/images/samsung/tabE.jpg">
+        <p>
+            Samsung Galaxy Tab E
+        </p>
+        <div>
+            10.000.000 vnd
+        </div>
+</div>
+<div id="box">
+        <img src="GUI/images/samsung/tabS4.jpg">
+        <p>
+            Samsung Galaxy Tab S4
+        </p>
+        <div>
+            10.000.000 vnd
+        </div>
+</div>
+
+
+<!--NOKIA-->
+<div id="box">
+        <img src="GUI/images/nokia/nokia2.1.jpg">
+        <p>
+            Nokia 2.1
+        </p>
+        <div>
+            10.000.000 vnd
+        </div>
+</div>
+<div id="box">
+        <img src="GUI/images/nokia/nokia2.jpg">
+        <p>
+            Nokia 2
+        </p>
+        <div>
+            10.000.000 vnd
+        </div>
+</div>
+<div id="box">
+        <img src="GUI/images/nokia/nokia3.1.jpg">
+        <p>
+            Nokia 3.1
+        </p>
+        <div>
+            10.000.000 vnd
+        </div>
+</div>
+<div id="box">
+        <img src="GUI/images/nokia/nokia5.jpg">
+        <p>
+            Nokia 5
+        </p>
+        <div>
+            10.000.000 vnd
+        </div>
+</div>
+<div id="box">
+        <img src="GUI/images/nokia/nokia6.1.jpg">
+        <p>
+            Nokia 6.1
+        </p>
+        <div>
+            10.000.000 vnd
+        </div>
+</div>
+<div id="box">
+        <img src="GUI/images/nokia/nokia6.1plus.jpg">
+        <p>
+            Nokia 6.1 Plus
+        </p>
+        <div>
+            10.000.000 vnd
+        </div>
+</div>
+<div id="box">
+        <img src="GUI/images/nokia/nokia6.jpg">
+        <p>
+            Nokia 6
+        </p>
+        <div>
+            10.000.000 vnd
+        </div>
+</div>
+<div id="box">
+        <img src="GUI/images/nokia/nokia7plus.jpg">
+        <p>
+            Nokia 7 Plus
+        </p>
+        <div>
+            10.000.000 vnd
+        </div>
+</div>
+<div id="box">
+        <img src="GUI/images/nokia/nokia3310.jpg">
+        <p>
+            Nokia 3310
+        </p>
+        <div>
+            10.000.000 vnd
+        </div>
+</div>
+<div id="box">
+        <img src="GUI/images/nokia/nokia8110.jpg">
+        <p>
+            Nokia 8110
         </p>
         <div>
             10.000.000 vnd
