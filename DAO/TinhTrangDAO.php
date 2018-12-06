dcb1737e03a576e1927d6bdd690508fbdf392fda
chinh trung <chinhtrung1998@gmail.com>
2018-12-06 10:13:50 +0700

    update TinhTrangDAO

--- DAO/TinhTrangDAO.php
+++ DAO/TinhTrangDAO.php
@@ -34,7 +34,36 @@
         }
         public function GetByID($MaTinhTrang)
         {
-            
+            $sql= "select MaTinhTrang, TenTinhTrang where MaTinhTrang=$MaTinhTrang";
+            $result=$this->ExecuteQuery($sql);
+            if($result==null)
+                return null;
+            $row= mysqli_fetch_array($result)
+            extract($row);
+            $tinhTrang= new TinhTrangDTO();
+            $tinhTrang->MaTinhTrang=$MaTinhTrang;
+            $tinhTrang->TenTinhTrang=$TenTinhTrang;
+            return $tinhTrang;
+
         }
+
+        public function Insert($tinhTrang)
+        {
+            $sql= "insert into tinhtrang(TenTinhTrang) values('$tinhTrang->TenTinhTrang)";
+            $this->ExecuteQuery($sql);
+        }
+
+        public function Delete($tinhTrang)
+        {
+            $sql= "delete from tinhtrang where MaTinhTrang=$tinhTrang->MaTinhTrang";
+            $this->ExecuteQuery($sql);
+        }
+
+        public function Update($tinhTrang)
+        {
+            $sql = "update tinhtrang set TenTinhTrang = $tinhTrang->TenTinhTrang where $tinhTrang->MaTinhTrang"
+            $this->ExecuteQuery($sql);
+        }
+        
     }
 ?>
